d1b43172bbdbdbd63b5a3315ccdc3d0b257a4e4a
Sylvain DNS <sylvain.denyse@gmail.com>
2019-07-10 11:10:32 +0200

    init db

---
+++ db/sql/01-create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE product ( 
+    product_id TEXT,
+    title TEXT,
+    price TEXT,
+    imageUrl TEXT,
+    productUrl TEXT,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    playedAt DATETIME,
+    CONSTRAINT product_pk PRIMARY KEY (product_id)
+);
